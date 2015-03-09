e365555f349834619d09447f8cae519037a74954
Simon B. <dragule.zergule@hotmail.fr>
2015-03-09 08:44:56 +0100

    -modelio
    retiré tous les commentaires de merde qu'a mit Modelio

--- Declaration.java
+++ Declaration.java
@@ -1,18 +1,11 @@
-import com.modeliosoft.modelio.javadesigner.annotations.objid;
-
-@objid ("6f5d23cf-3032-43d0-ba31-dfbd589ff414")
 public class Declaration {
-    @objid ("31a34654-c06b-4854-8d7d-ddd332020c60")
     public String identLu;
 
-    @objid ("2ccd1238-102a-4af7-808e-38217a70c89e")
     public String typeLu;
 
-    @objid ("21f1134e-b76f-436a-b4ef-0bc8089d6281")
     public void placerVariable(String nom, String val) {
     }
 
-    @objid ("e21f1967-43cb-48e6-9dad-69cc4f112714")
     public void placerConst(String nom, String val) {
     }
 
--- TabIdent.java
+++ TabIdent.java
@@ -1,27 +1,17 @@
-import java.util.Map;
-import com.modeliosoft.modelio.javadesigner.annotations.objid;
-
-@objid ("b51465a8-2406-43f0-be45-4abaaa5435c1")
 public class TabIdent {
-    @objid ("b82747bb-a539-4165-b933-4de76404bbb2")
     private Map<string, String> table;
 
-    @objid ("906cec74-69a6-441a-bf9e-802408fd5c21")
     public Yaka yaka;
 
-    @objid ("513bd558-7eda-48d7-bb18-5e8af44b884b")
     public TabIdent(int taille) {
     }
 
-    @objid ("1ecc3629-93ce-468d-a95e-ef90bbf58bdc")
     public Ident chercheIdent(String clef) {
     }
 
-    @objid ("32d513be-303d-4fe7-932b-fb9d0633d2f8")
     public boolean existeIdent(String clef) {
     }
 
-    @objid ("755d6d70-aacd-4ae0-8f8a-12c3a0689a9e")
     public void rangeIdent(String clef, String id) {
     }
 
