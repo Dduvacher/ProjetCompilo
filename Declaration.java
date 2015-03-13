8b383827877779de8ce6a0b0db61aa931ce37dcb
unknown <sbouvier@PC2-INFO4.educ.insa>
2015-03-13 16:59:24 +0100

    fini ?

--- Declaration.java
+++ Declaration.java
@@ -2,18 +2,32 @@
 
 public class Declaration implements Constante {
     public String identLu;
-
     public String typeLu;
+    public YVM yvm;
+    public YVMasm asm;
+    
+    public Declaration (YVM yvm, YVMasm asm){
+    	this.yvm = yvm;
+    	this.asm = asm;
+    }
 
     public void placerVariable(String nom, int val, type type, TabIdent tab) {
     	if (tab.existeIdent(nom)){
-    	
+    		System.out.println("ERROR : IDENT ALREADY EXISTS");
     	} else {
     		tab.rangeIdent(nom, new IdVar(type, val));
+    		yvm.istore(val);
     	}
     }
 
-    public void placerConst(String nom, String val) {
+    public void placerConst(String nom, int val, type type, TabIdent tab) {
+    	if (tab.existeIdent(nom)){
+    		System.out.println("ERROR : IDENT ALREADY EXISTS");
+    	} else {
+    		tab.rangeIdent(nom, new IdConst(type, val));
+    		yvm.iconst(val);
+    		
+    	}
     }
 
 }
