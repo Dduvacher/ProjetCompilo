d54e3203718ad8fc45ad41e69c115eae0bc1d95d
unknown <dduvache@PC2-INFO3.educ.insa>
2015-03-13 17:10:50 +0100

    Yaka.jj et tou ce qui va avec

--- Declaration.java
+++ Declaration.java
@@ -5,7 +5,7 @@ public class Declaration implements Constante {
     public String typeLu;
     public YVM yvm;
     public YVMasm asm;
-    
+    //tasoeur
     public Declaration (YVM yvm, YVMasm asm){
     	this.yvm = yvm;
     	this.asm = asm;
