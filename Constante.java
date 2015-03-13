8a37dcc8ae3dbe9259561347a109bef9e79130b6
unknown <sbouvier@PC2-INFO4.educ.insa>
2015-03-13 16:08:11 +0100

    creation enum types

--- Constante.java
+++ Constante.java
@@ -1,12 +1,7 @@
 //import com.modeliosoft.modelio.javadesigner.annotations.objid;
 /*test*/
 public class Constante {
-    public int entier;
-
-    public String booleen;
-
-    public String operateur;
-
-    public int error;
+    
+    enum type {BOOL, ENTIER, ERREUR};
 
 }
